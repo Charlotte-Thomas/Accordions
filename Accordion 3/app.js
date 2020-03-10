1afd63039c2cd60b99f04f9a76a91e259cbe09f6
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-03-10 18:17:43 +0000

    acc3 zoom effect added

--- Accordion 3/app.js
+++ Accordion 3/app.js
@@ -5,12 +5,16 @@ function setupApp() {
 
   accordion.forEach((sec, index) => {
     sec.addEventListener('mouseenter', () => {
+      sec.firstChild.style.display = 'block'
       sec.firstChild.classList.remove('hide')
       sec.firstChild.classList.add('text')
     })
     sec.addEventListener('mouseleave', () => {
       sec.firstChild.classList.add('hide')
       sec.firstChild.classList.remove('text')
+      setTimeout(() => {
+        sec.firstChild.style.display = 'none'
+      }, 800)
     })
   })
 
